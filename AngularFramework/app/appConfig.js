907acc6860215f51e32ea88549c8ef618405b394
ashafir <ashafir20@gmail.com>
2015-07-19 23:06:50 +0300

    started the menu module

---
+++ AngularFramework/app/appConfig.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+angular.module("app").config(function($provide) {
+    $provide.decorator("$exceptionHandler", [
+        "$delegate", function($delegate) {
+            return function(excepetion, cause) {
+                $delegate(excepetion, cause);
+                //alert(excepetion.message);
+            }
+        }
+    ]);
+});
