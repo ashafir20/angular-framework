a65d89803012d68d954761296cbc60e4a81191d0
ashafir <ashafir20@gmail.com>
2015-07-22 20:16:44 +0300

    started adding routing for menu item

--- AngularFramework/ext-modules/psFramework/psFramworkController.js
+++ AngularFramework/ext-modules/psFramework/psFramworkController.js
@@ -3,6 +3,9 @@
 angular.module("psFramework").controller("psFramworkController",
     ["$scope",
     function ($scope) {
-        
+
+        $scope.$on('ps-menu-item-selected-event', function(evt, data) {
+            $scope.routeString = data.route;
+        });
     }]
 );
