c02580b459e81762ff07d239c91979e8be89f433
ashafir <ashafir20@gmail.com>
2015-07-24 00:22:14 +0300

    finished module 2 menu

--- AngularFramework/ext-modules/psFramework/psFramworkController.js
+++ AngularFramework/ext-modules/psFramework/psFramworkController.js
@@ -1,11 +1,47 @@
 ﻿"use strict";
 
 angular.module("psFramework").controller("psFramworkController",
-    ["$scope",
-    function ($scope) {
+    ["$scope", '$window', '$timeout','$rootScope',
+    function ($scope, $window, $timeout, $rootScope) {
+
+        $scope.isMenuVisible = true;
+        $scope.isMenuButtonVisible = true;
+
+        var checkWidth = function () {
+            var width = Math.max($($window).width(), $window.innerWidth);
+            $scope.isMenuVisible = (width > 768);
+            $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+        }
+
+        $($window).on('resize.psFramework', function() {
+            $scope.$apply(function() {
+                checkWidth();
+                broadcastMenuState();
+            });
+        });
+
+        $scope.$on('$destroy', function() {
+            $($window).off("resize.psFramework");  //remove the handler
+        });
 
         $scope.$on('ps-menu-item-selected-event', function(evt, data) {
             $scope.routeString = data.route;
+            checkWidth();
+            broadcastMenuState();
         });
+
+        $scope.menuButtonClicked = function() {
+            $scope.isMenuVisible = !$scope.isMenuVisible;
+            broadcastMenuState();
+            //$scope.$apply();
+        }
+
+        var broadcastMenuState = function() {
+            $rootScope.$broadcast('ps-menu-show', { show: $scope.isMenuVisible });
+        }
+
+        $timeout(function() {
+            checkWidth();
+        }, 0);
     }]
 );
--- AngularFramework/ext-modules/psMenu/psMenuController.js
+++ AngularFramework/ext-modules/psMenu/psMenuController.js
@@ -2,7 +2,14 @@
 
 angular.module('psMenu').controller("psMenuController",
     ['$scope', '$rootScope',
-    function($scope, $rootScope) {
+    function ($scope, $rootScope) {
+
+        $scope.showMenu = true;
+
+        this.getActiveElement = function() {
+            return $scope.activeElement;
+        }
+
         this.setActiveElement = function(el) {
             $scope.activeElement = el;
         };
@@ -10,5 +17,9 @@ angular.module('psMenu').controller("psMenuController",
         this.setRoute = function(route) {
             $rootScope.$broadcast('ps-menu-item-selected-event', { route: route });
         };
+
+        $scope.$on('ps-menu-show', function(evt, data) {
+            $scope.showMenu = data.show;
+        });
     }
 ]);
